568aaa50180d0ec4a330ca699a8d47bfe2f211df
MyagkiyBogdan <93487432+MyagkiyBogdan@users.noreply.github.com>
2022-05-22 19:11:51 +0000

    Deploying to gh-pages from @ MyagkiyBogdan/js-mini-projects@27434ca16226b924dc2da53db6375a2556f7713c 🚀

--- src.c0b6e899.js
+++ src.c0b6e899.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/js-mini-projects/src.0a81e7a8.js.map
+//# sourceMappingURL=/js-mini-projects/src.c0b6e899.js.map
